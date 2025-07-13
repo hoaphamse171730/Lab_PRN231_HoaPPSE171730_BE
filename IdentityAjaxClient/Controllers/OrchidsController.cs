313b0a1b77d7efd8360d7de1b4948d27badcc60c
Phan Pham Hoa <hoappse171730@fpt.edu.vn>
2025-07-13 15:44:34 +0700

    feat: update UI

---
+++ IdentityAjaxClient/Controllers/OrchidsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAjaxClient.Controllers
+{
+    [Authorize]
+    public class OrchidsController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
